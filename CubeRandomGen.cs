b3e77a5f3c8fe9c10039be2f5df4195c702fbca3
Frityet <ambhogal01@gmail.com>
2021-07-11 16:02:14 -0700

    Cleaned code

--- CubeRandomGen.cs
+++ CubeRandomGen.cs
@@ -2,42 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+
+
 [ExecuteInEditMode]
-class RandomGenWindow : EditorWindow {
-	Object spawnableObject;
-	Object targetCube;
-	int numberOfEncryptions = 1;
+public class RandomGenWindow : EditorWindow 
+{
+	public Object ObjectToSpawn;
+	public Object TargetCube;
+	public int EncryptionCount = 1;
 	
 	[MenuItem ("Tools/Random Encryption Placer")]
-	
-	public static void ShowWindow () {
+	public static void ShowWindow() 
+	{
 		EditorWindow.GetWindow(typeof(RandomGenWindow));
 	}
 	
-	void OnGUI () {
-		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
-		spawnableObject = EditorGUILayout.ObjectField("Object to Clone:", spawnableObject, typeof(GameObject), true);
-		targetCube = EditorGUILayout.ObjectField("Target Cube:", targetCube, typeof(GameObject), true);
-		numberOfEncryptions = EditorGUILayout.IntSlider("Object Count:", numberOfEncryptions, 1, 100);
-		if (GUILayout.Button("Spawn Objects")) {
-///			if (spawnableObject == null)
-///				this.ShowNotification(GUIContent("No spawnable object selected."));
-///			else if (targetCube == null)
-///				this.showNotification(GUIContent("No target cube selected"));
-///			else
+	void OnGUI() 
+	{
+		GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+		ObjectToSpawn = EditorGUILayout.ObjectField("Object to Clone:", ObjectToSpawn, typeof(GameObject), true);
+		TargetCube = EditorGUILayout.ObjectField("Target Cube:", TargetCube, typeof(GameObject), true);
+		EncryptionCount = EditorGUILayout.IntSlider("Object Count:", EncryptionCount, 1, 100);
+		if (GUILayout.Button("Spawn Objects")) 
+		{
+			for (int i = 0; i < EncryptionCount; i++)
 			{
-				for (int i = 0; i < numberOfEncryptions; i++)
-				{
-					spawnObject((GameObject)spawnableObject, (GameObject)targetCube);
-				}
+				SpawnObject(ObjectToSpawn, TargetCube);
 			}
 		}
 	}
 	
-	public void spawnObject(GameObject spawnableThing, GameObject targetLocation)
+	public void SpawnObject(Object spawnableThing, Object targetLocation)
 	{
-		Vector3 rndPosWithin;
-		rndPosWithin = targetLocation.transform.TransformPoint(Random.Range(-1f, 1f) * .5f, Random.Range(-1f, 1f) * .5f, Random.Range(-1f, 1f) * .5f);
+		Vector3 rndPosWithin = targetLocation.transform.TransformPoint(Random.Range(-1f, 1f) * .5f, Random.Range(-1f, 1f) * .5f, Random.Range(-1f, 1f) * .5f);
 		Instantiate(spawnableThing, rndPosWithin, targetLocation.transform.rotation);
 	}
 }
